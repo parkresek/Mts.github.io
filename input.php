80f29a363fbfb8ae1a4b0eefc66dad063f32f3ec
parkresek <antowiranto47@gmail.com>
2022-02-07 22:56:53 +0800

    day 58

--- input.php
+++ input.php
@@ -1,30 +1,44 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>form</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
 </head>
 
 <body>
-    <form action="/action_page.php">
-        <div class="mb-3 mt-3">
-            <label for="email" class="form-label">Email:</label>
-            <input type="email" class="form-control" id="email" placeholder="Enter email" name="email">
+    <div class="container" style="margin-top: 20px;">
+        <div class="row">
+            <div class="col-sm-3" style="margin-top: 50px;">
+                <img src="img/ikmt.png" alt="" width="200" height="200">
+            </div>
+            <div class="col-sm-9">
+                <form>
+                    <div class="mb-3">
+                        <label for="exampleInputEmail1" class="form-label">Email address</label>
+                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                        <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleInputPassword1" class="form-label">Password</label>
+                        <input type="password" class="form-control" id="exampleInputPassword1">
+                    </div>
+                    <div class="mb-3 form-check">
+                        <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                        <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </form>
+            </div>
         </div>
-        <div class="mb-3">
-            <label for="pwd" class="form-label">Password:</label>
-            <input type="password" class="form-control" id="pwd" placeholder="Enter password" name="pswd">
-        </div>
-        <div class="form-check mb-3">
-            <label class="form-check-label">
-                <input class="form-check-input" type="checkbox" name="remember"> Remember me
-            </label>
-        </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
-    </form>
+    </div>
+
 </body>
 
 </html>
--- koneksi.php
+++ koneksi.php
@@ -3,7 +3,7 @@ $servername = "localhost";
 $database = "Mts";
 $username = "root";
 $password = "password";
- 
+
 // untuk tulisan bercetak tebal silakan sesuaikan dengan detail database Anda
 // membuat koneksi
 $conn = mysqli_connect($servername, $username, $password, $database);
@@ -13,4 +13,25 @@ if (!$conn) {
 }
 echo "Koneksi berhasil";
 mysqli_close($conn);
-?>
+
+
+//menerima nilai dari kiriman form pendaftaran
+$email = $_POST["email"];
+$password = $_POST["password"]; //untuk password digunakan enskripsi md5
+
+
+//Query input menginput data kedalam tabel anggota
+$sql = "insert into user (email,password) values
+		('$email','$password')";
+
+//Mengeksekusi/menjalankan query diatas	
+$hasil = mysqli_query($conn, $sql);
+
+//Kondisi apakah berhasil atau tidak
+if ($hasil) {
+    echo "Berhasil simpan data anggota";
+    exit;
+} else {
+    echo "Gagal simpan data anggota";
+    exit;
+}
